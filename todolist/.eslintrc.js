79f602627748c57893f7feda286b1d6dc8a8a350
Gabriel Mello <gabriel.mellosjb@gmail.com>
2023-06-15 10:42:34 -0300

    Eslint and prettier config

---
+++ todolist/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+    requireConfigFile: false,
+  },
+  plugins: ["react", "prettier", "react-hooks"],
+  rules: {
+    "prettier/prettier": 2,
+    "react/jsx-filename-extension": "off",
+    "import/prefer-default-export": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/react-in-jsx-scope": "off",
+    "no-undef": "off",
+  },
+};
