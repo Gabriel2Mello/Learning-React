8aa3d51327eeeeddf5e2ee2442be13e2039901ed
Gabriel Mello <gabriel.mellosjb@gmail.com>
2023-09-07 17:23:43 -0300

    Tasks style

--- todolist/src/components/Main.js
+++ todolist/src/components/Main.js
@@ -1,21 +1,52 @@
+import "./Main.css";
+import "./Form/Form";
+import "./Tasks/Tasks";
+
 import React, { Component } from "react";
+import { FaPlus } from "react-icons/fa";
+import { FaEdit, FaWindowClose } from "react-icons/fa";
 
 export default class Main extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    newTask: "",
+    tasks: ["tomar", "beber"],
+  };
 
-    this.state = {};
-  }
+  handleInputChange = (e) => {
+    this.setState({
+      newTask: e.target.value,
+    });
+  };
 
   render() {
+    const { newTask, tasks } = this.state;
+
     return (
       <div className="main">
         <h1>Todo list</h1>
 
-        <form action="#">
-          <input type="text" />
-          <button type="submit">Add</button>
+        <form action="#" className="form">
+          <input
+            onChange={this.handleInputChange}
+            type="text"
+            value={newTask}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+
+        <ul className="tasks">
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
